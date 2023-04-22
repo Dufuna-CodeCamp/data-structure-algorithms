88658bc64f214f0383e940f4f1e316bc86b19d9d
TunrayoIlawole <tunrayoilawole99@gmail.com>
2023-04-22 11:05:33 +0100

    add valid palindrom solution

---
+++ dsa-pros/leetcode/string/java/motunrayoIlawole/valid-palindromeii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        if (isPalindrome(s, left, right)) {
+            return true;
+        }
+
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            }
+            else {
+                return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
