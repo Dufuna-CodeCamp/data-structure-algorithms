4c6a910933f8aa879e94f625d37179e197dd94cf
Timilehin08 <timilehindebra@gmail.com>
2023-04-24 21:53:55 +0100

    worked on decode string using stack queues

--- dsa-pros/leetcode/string/javascript/timiBello/stringsSoln2.js
+++ dsa-pros/leetcode/string/javascript/timiBello/stringsSoln2.js
@@ -130,3 +130,30 @@ var partitionString = function(s) {//3
 /**
   * @param {string} s
   * @return {number} **/
+ 
+var decodeString = function (s) {
+  const N = s.length
+
+  const stack = []
+
+  for (let i = 0; i < N; i += 1) {
+    const ch = s[i]
+
+    if (ch === ']') {
+      let sub = ''
+      while (stack.at(-1) !== '[') {
+        sub = stack.pop() + sub
+      }
+      stack.pop()
+      let num = ''
+      while (!isNaN(stack.at(-1))) {
+        num = stack.pop() + num
+      }
+      stack.push(sub.repeat(num))
+    } else {
+      stack.push(ch)
+    }
+  }
+
+  return stack.join('')
+}
