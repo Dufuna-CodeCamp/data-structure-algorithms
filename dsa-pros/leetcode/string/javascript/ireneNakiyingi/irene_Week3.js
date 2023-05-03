192a9171b411a69607aec7069758d3ce5f06a6aa
Irene Nakiyingi <iryntracy@gmail.com>
2023-05-03 15:24:55 +0300

    no 2 edit

--- dsa-pros/leetcode/string/javascript/ireneNakiyingi/irene_Week3.js
+++ dsa-pros/leetcode/string/javascript/ireneNakiyingi/irene_Week3.js
@@ -44,8 +44,44 @@ var balancedStringSplit = function(s) {
 
 //2.Multiply strings
 wow
+//run time 69ms memeory 43.8mb
+//space and time??
+ // how to get each value out of string then multiply resulting value to get ans
+ // cannot use int()*
+ /* let product = num1;
+ product *= num2;
+ return (""+Math.floor(product)+"") works but not correct*/
+// trial 3 use stacks
+var multiply = function(num1, num2) {
+ const stack = [];
+ num1 = num1.split('').reverse(); // since multiplication os done right to left nit left to right
+ num2 = num2.split('').reverse();
+
+ for (i=0;i<num1.length;i++){ //iterate throuhgh num1
+     for (j=0;j<num2.length;j++){ //iterate through num2
+         var product = num1[i]*num2[j];
+         // adding to the stack
+         var stackAdd = stack[i+j];
+         stack[i+j]= stackAdd ? stackAdd + product: product;
+     }
+    
+ } 
+ // for loop through the stack
+     for(i=0;i< stack.length;i++){
+         const stepStack = Math.floor(stack[i]/10);
+         stack[i]= stack[i]%10; //return whole numbers ie carry
+         if (stack[i+1]){
+             stack[i+1] += stepStack;
+         } else if(stepStack>0){
+             stack[i+1] = stepStack;
+         }
+     }
+     // had to add replace to remove repeated values 
+ return stack.reverse().join('').replace(/^(0(?!$))+/, "")
+};
 //3.re arrange strings
-eh
+//eh 
+//in progress
 
 
 //4 Regular expression matching ------ hard
