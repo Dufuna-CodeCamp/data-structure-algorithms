153f32195f5d1b7f302cbbf1ea272067c3e622bc
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2023-08-09 15:59:56 +0100

    Linked List solutions (#53)
    * add valid anagram solution
    
    * add valid palindrom solution
    
    * add longest-substring solution
    
    * add valid palindrom solution
    
    * add optimal partition string solution
    
    * add decode string solution
    
    * add comments
    
    * update
    
    * update
    
    * update
    
    * add array solutions
    
    * Add array solutions
    
    * add array solution
    
    * linked list solutions
    
    * linked list solution
    
    * linked list solution
    
    * add linked-list solution
    
    * add linked-list solution
    
    * add linked-list solution
    
    * add linked-list solution
    
    * add linked-list solution
    
    * add linked list solution

---
+++ dsa-pros/leetcode/linked-list/motunrayoIlawole/intersection-of-linked-lists.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        while (headA != null) {
+            ListNode temp = headB;
+
+            while (temp != null) {
+                if (headA == temp) {
+                    return headA;
+                }
+                temp = temp.next;
+            }
+
+            headA = headA.next;
+        }
+        return null;
+        
+    }
+}
