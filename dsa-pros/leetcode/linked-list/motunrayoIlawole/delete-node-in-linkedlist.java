f3c4fc6a1a35669afdc170da6c49702a426cd9a0
TunrayoIlawole <tunrayoilawole99@gmail.com>
2023-08-09 07:32:30 +0100

    add linked list solution

--- dsa-pros/leetcode/linked-list/motunrayoIlawole/delete-node-in-linkedlist.java
+++ dsa-pros/leetcode/linked-list/motunrayoIlawole/delete-node-in-linkedlist.java
@@ -1,11 +1,11 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+//   Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
 class Solution {
     public void deleteNode(ListNode node) {
 
