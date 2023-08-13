63b61461c6cf8b385ab3eba1fb5384269473736b
Timilehin08 <timilehindebra@gmail.com>
2023-08-13 16:54:52 +0100

    linkedListquestions

---
+++ dsa-pros/leetcode/linked-list/javascript/timiBello/Wk12LinkedList.js
@@ -0,0 +1,54 @@
+//Middle of the linked list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return slow;
+};
+
+
+//Remove linked list elements 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    const dummy = new ListNode(-1);
+    dummy.next = head;
+    let current = dummy;
+
+    while (current.next) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return dummy.next;
+};
