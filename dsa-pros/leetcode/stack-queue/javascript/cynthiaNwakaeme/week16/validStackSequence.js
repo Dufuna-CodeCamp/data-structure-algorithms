a45219b4238a341c63b87c20340741c5d0e3d4ec
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2023-08-28 12:18:49 +0100

    Week16

--- dsa-pros/leetcode/stack-queue/javascript/cynthiaNwakaeme/week16/validStackSequence.js
+++ dsa-pros/leetcode/stack-queue/javascript/cynthiaNwakaeme/week16/validStackSequence.js
@@ -5,14 +5,26 @@
  */
 
 const validStackSequence = (pushed, popped) => {
-  if(pushed.length !== popped.length) return false;
+  /*if(pushed.length !== popped.length) return false;
 
   for(let i = 1; i <= pushed.length ; i++) {
     if((pushed[i - 1] > popped[popped.length - i])) return false;
   }
 
   return true;
+*/
+  const stack = []
+  let i = 0
 
+  for (const element of pushed) {
+    stack.push(element);
+
+    while (stack.length && stack[stack.length - 1] === popped[i]) {
+      stack.pop()
+      i++
+    }
+  }
+
+  return pushed.length === i;
 };
 
-console.log(validStackSequence([1,2,3,4,5], [4,5,3,2,1]))
