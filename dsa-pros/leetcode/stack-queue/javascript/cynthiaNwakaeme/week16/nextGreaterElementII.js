b65d0fd7b694be51964e985db0aa3eca2a7bb3e3
Cynthia Ebere Nwakaeme <nwakaemecynthia@gmail.com>
2023-09-17 12:33:46 +0100

    completed week 16 task (#66)
    * Solution to PalindromeII and Optimal-Partition  Quiz
    
    * Week12 - Exercise1
    
    * Week12 - Exercise2
    
    * Week11 - Exercise1
    
    * Week15 - Exercise1
    
    * Week15 - Exercise2
    
    * Week15 - improved getMinValue of Exercise2
    
    * Week15 - improved getMinValue of Exercise2 and implemented Exercise3
    
    * Week16 - Exercise1
    
    * Week16 - improved Exercise1 and implemented Exercise2
    
    * Week16 - improved Exercise1 and implemented Exercise2
    
    * Week16

--- dsa-pros/leetcode/stack-queue/javascript/cynthiaNwakaeme/week16/nextGreaterElementII.js
+++ dsa-pros/leetcode/stack-queue/javascript/cynthiaNwakaeme/week16/nextGreaterElementII.js
@@ -1,3 +1,32 @@
-const nextGreaterElementII = () => {
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const nextGreaterElementII = (nums) => {
+  let i = 0
+  let output = []
 
+  const incrementIndex = (index) => index === nums.length - 1 ? 0 : index + 1
+
+  let startIndex = incrementIndex(i)
+
+  while(output.length < nums.length) {
+    if(nums[startIndex] > nums[i]) {
+      output.push(nums[startIndex])
+      i = incrementIndex(i)
+      startIndex = incrementIndex(i)
+    } else {
+      if(startIndex !== i) {
+        startIndex = incrementIndex(startIndex)
+      } else {
+        output.push(-1)
+        i = incrementIndex(i)
+        startIndex = incrementIndex(i)
+      }
+    }
+  }
+
+  return output;
 }
+
+console.log(nextGreaterElementII([1,2,1]));
